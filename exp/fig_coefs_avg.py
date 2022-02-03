09465269102649707b1b118fef5d1e311a62cf84
Zleander <zimmermannleander@gmail.com>
2022-02-03 18:32:09 +0100

    updating structure and naming to fit the requirements

---
+++ exp/fig_coefs_avg.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from tueplots import bundles
+
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt_settings = bundles.neurips2021(usetex=False)
+plt_settings['figure.constrained_layout.use'] = False
+plt.rcParams.update(plt_settings)
+
+fig_width, fig_height = plt_settings['figure.figsize']
+
+# data:
+interesting_features = ['explicit', 'danceability','loudness','valence']
+predictors = ['explicit', 'danceability', 'energy', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
+coef_ = pd.read_csv('../dat/log_reg_coef.csv', index_col=0).to_numpy()
+train = pd.read_csv('../dat/train_set.csv')
+
+# start of script
+fig = plt.figure(figsize=(fig_width, fig_height*1.2))
+gs = fig.add_gridspec(2, hspace=0)
+axs = gs.subplots(sharex=True, sharey=True)
+ax1, ax2 = axs
+
+year_series = np.unique((np.unique(train['year'])))
+
+for i,feature in enumerate(predictors):
+    if feature in interesting_features:
+        ax1.plot(year_series, coef_[:,i], '.-', label=feature,)
+    else:
+        ax1.plot(year_series, coef_[:,i], '-', alpha=.3,color='grey')
+    
+
+train_ = train[predictors + ['year']]
+train_[predictors] = preprocessing.StandardScaler().fit_transform(train_[predictors])
+
+tracks_by_year_avg = train_.groupby('year').mean()
+
+
+for i,feature in enumerate(predictors[:-1]):
+    if feature in interesting_features:    
+        ax2.plot(year_series,tracks_by_year_avg[feature], '.-',label=feature )
+    else:
+        ax2.plot(year_series,tracks_by_year_avg[feature], '-', alpha=.3, color='grey')
+ax2.plot(year_series,tracks_by_year_avg[predictors[-1]], '-', alpha=.3, color='grey', label='other')
+
+
+ax2.set_xlabel('Year')
+ax2.set_ylabel('Avg. Feature Value')
+ax1.set_ylabel('Coefficient Value')
+for ax in axs:
+    #ax.label_outer()
+    ax.set_ylim(-.75, .5)
+    ax.grid(alpha=.2)
+ax2.legend(loc=(.4,0.85), framealpha=1)
+#ax2.legend(loc= 'lower center')
+
+ax1.tick_params(axis='x', which='both', bottom=False,labelbottom=False)
+
+plt.savefig('../doc/fig/coefs_avg.pdf',bbox_inches='tight')
+plt.show()
---
+++ exp/fig_losses_lincoefs.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+from tueplots import bundles
+
+plt_settings = bundles.neurips2021(usetex=False)
+plt.rcParams.update(plt_settings)
+
+fig_width, fig_height = plt_settings['figure.figsize']
+
+# data 1:
+names = ['Random', 'Lin. Regression', 'Poly. Lin. Regression', 'Log. Regression', 'Poly. Log. Regression']
+l1_losses = [10.129583746499913, 6.113696974889281, 5.956544964282252, 7.596235415401686, 7.7255190609943085]
+l2_losses = [153.8909257698003, 53.87570765992985, 51.64688522673475, 95.0179798447513, 98.38477129864627]
+accuracy = [0.031334553539836545, 0.04506318718521075, 0.04871070185171433, 0.07885620929308108, 0.07910520131223163]
+
+# data 2:
+interesting_features = ['explicit', 'danceability','loudness','valence']
+predictors = ['explicit', 'danceability', 'energy', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
+coef_ = pd.read_csv('../dat/lin_reg_coef.csv', index_col=0).to_numpy()
+
+N = len(names)
+ind = np.arange(N)
+fig, axs = plt.subplots(1,2, gridspec_kw={'width_ratios': [2, 3]})
+ax, ax2 = axs
+ax1 = ax.twinx()
+                       
+width = 0.3       
+
+# TODO change these colors
+color0 = 'red'
+color1 = 'green'
+
+# Plotting
+b0 = ax.bar(ind, l1_losses , width, label='$L_1$-Loss (years)', color=color0)
+#b0 = ax.bar(ind, l2_losses , width, label='$L_2$-Loss (years)', color=color0)
+b1 = ax1.bar(ind + width, accuracy, width, label='Accuracy', color=color1)
+ax.set_xticks(ind + width / 2, names, rotation=45, horizontalalignment='right')
+
+ax.legend([b0,b1],[b.get_label() for b in [b0, b1]], loc='upper left')
+
+ax.tick_params(axis='y', labelcolor=color0)
+accs = np.array(list(range(0,9,1)))
+ax1.set_yticks(accs/100, [f'{a}%' for a in accs])
+ax1.tick_params(axis='y', labelcolor=color1)
+ax.set_ylim(0,12.5)
+
+interesting_features = ['explicit', 'danceability','loudness','valence']
+for f, c in zip(predictors,coef_):
+    ax2.bar(f, c, color='grey' if f not in interesting_features else None)
+plt.setp(ax2.get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.savefig('../doc/fig/losses_lincoefs.pdf',bbox_inches='tight')
+plt.show()
